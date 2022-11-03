6093911bb14869ecf85af364a789ac11781ea85b
dsvivass <dsvivass@unal.edu.co>
2022-11-03 17:47:19 -0500

    Tests automated on Github actions

---
+++ api/tests/test_basic.py
@@ -0,0 +1,18 @@
+import json
+from unittest import TestCase
+
+from api.app import app
+
+
+class TestBasic(TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_hello_world(self):
+        '''Test that the hello world route returns a 200 status code and a specific message'''
+        
+        request = self.client.get('/api/test')
+        response =json.loads(request.get_data())
+        
+        self.assertEqual(request.status_code, 200)        
